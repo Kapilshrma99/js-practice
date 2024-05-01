f11c23c7d986c8d15ce45488719b9cfe8038e8aa
Kapilshrma99 <96725122+Kapilshrma99@users.noreply.github.com>
2024-05-01 11:36:44 +0000

    practice

---
+++ 02_basics/arr1.js
@@ -0,0 +1,6 @@
+const myArr=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(myArr.push(11));
+// console.log(myArr.pop());
+// myArr.unshift(9)
+console.log(myArr.slice(0, 3));
+console.log(myArr);
---
+++ 02_basics/arr2.js
@@ -0,0 +1,4 @@
+let a=["a","b","c"];
+let b=["a","b","c"];
+result=[...a, ...b];
+console.log(result.sort());
